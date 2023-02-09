91d1b3c527fd9c492944f959cf5c4b84a071237b
frknsyl <frknsyl88@gmail.com>
2023-02-10 00:09:35 +0300

    Category Updates

---
+++ dada/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Categories from './components/categories';
+import NavBar from './components/navbar'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar/>
+      <hr color='#B4846C'/>
+      <Categories/>
+    </div>
+  )
+}
+
+export default App;
